af11ce85d1492109411fcf990055699b2fa36131
juleni <69912179+juleni@users.noreply.github.com>
2022-03-17 13:09:32 +0100

    ADD: Payment

---
+++ functions/index.js
@@ -0,0 +1,58 @@
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const { async } = require("@firebase/util");
+const { default: Stripe } = require("stripe");
+const stripe = require("stripe")(
+  "sk_test_51KdzwaF9THjM6oCRJdgMkVEffq9QjEP7gMqNGKhu6sGB1DJNH25RuNt1KsMcFFiuttLI2ZdyglATAL0P8WI0QzTK000wPz391i"
+);
+
+// API
+
+// - App config
+const app = express();
+
+// - Middleware
+// eslint-disable-next-line object-curly-spacing
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// - API routes
+// app.get("/", (req, res) => res.status(200).send("Hello world!"));
+// app.get("/juleni", (req, res) => res.status(200).send("Hello JULENI!"));
+app.post("/payments/create", async (req, res) => {
+  const total = req.query.total;
+  console.log("Received payment total: ", total);
+  if (total > 0) {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: total, // Specify amount here
+      currency: "usd", // Specify currency here
+    });
+
+    // Return client secret, status code and message to client
+    res.status(201).send({
+      clientSecret: paymentIntent.client_secret,
+      message: "Payment received",
+    });
+  } else {
+    // Return error status code and message to client
+    res.status(400).send({
+      message: "Payment cannot be 0.",
+    });
+  }
+});
+
+// - Listen command
+exports.api = functions.https.onRequest(app);
+
+// firebase emulators:start
+// example endpoint
+// http://localhost:5001/challenge-507b1/us-central1/api
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -6,10 +8,23 @@ import Checkout from "./Checkout";
 import Header from "./Header";
 import Home from "./Home";
 import Login from "./Login";
+import Orders from "./Orders";
+import Payment from "./Payment";
 import { useStateValue } from "./StateProvider";
+
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = loadStripe(
+  "pk_test_51KdzwaF9THjM6oCRXa6UW4k0cAqLOFLUe4FaNoo6gZk6hIUn1FTZcCtmK7i9KdlkQ2sPoBpN7AKIcIfyA6mpdpHL00QRqQUWUQ"
+);
 
 function App() {
   const [{ user }, dispatch] = useStateValue();
+
+  const options = {
+    // passing the client secret obtained from the server
+    clientSecret: "",
+  };
 
   useEffect(() => {
     // will only run once when the app component loads...
@@ -41,6 +56,19 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/checkout" element={<Checkout />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route
+            path="/payment"
+            element={
+              <>
+                <Elements stripe={stripePromise}>
+                  <Payment />
+                </Elements>
+              </>
+            }
+          />
+          {/* Default route to home page */}
+          <Route path="*" element={<Home />} />{" "}
           {/* <Route path="/" element={<Header />}>
             <Route path="home" element={<Home />} />
             <Route path="checkout" element={<Checkout />} />
