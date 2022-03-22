a409303005dd1f805d027728c1af7a05348b2297
juleni <69912179+juleni@users.noreply.github.com>
2022-03-22 11:35:52 +0100

    ADD: styles + fixes

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,6 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
-const { async } = require("@firebase/util");
 const { default: Stripe } = require("stripe");
 const stripe = require("stripe")(
   "sk_test_51KdzwaF9THjM6oCRJdgMkVEffq9QjEP7gMqNGKhu6sGB1DJNH25RuNt1KsMcFFiuttLI2ZdyglATAL0P8WI0QzTK000wPz391i"
@@ -21,23 +20,27 @@ app.use(express.json());
 // app.get("/", (req, res) => res.status(200).send("Hello world!"));
 // app.get("/juleni", (req, res) => res.status(200).send("Hello JULENI!"));
 app.post("/payments/create", async (req, res) => {
-  const total = Math.round(req.query.total);
-  if (total > 0) {
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: total, // Specify amount here
-      currency: "usd", // Specify currency here
-    });
+  try {
+    const total = Math.round(req.query.total);
+    if (total > 0) {
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: total, // Specify amount here
+        currency: "usd", // Specify currency here
+      });
 
-    // Return client secret, status code and message to client
-    res.status(201).send({
-      clientSecret: paymentIntent.client_secret,
-      message: "Payment received",
-    });
-  } else {
-    // Return error status code and message to client
-    res.status(400).send({
-      message: "Payment cannot be 0.",
-    });
+      // Return client secret, status code and message to client
+      res.status(201).send({
+        clientSecret: paymentIntent.client_secret,
+        message: "Payment received",
+      });
+    } else {
+      // Return error status code and message to client
+      res.status(400).send({
+        message: "Payment cannot be 0.",
+      });
+    }
+  } catch (err) {
+    console.log(err);
   }
 });
 
